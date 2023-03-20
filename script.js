7746e74ee2d1bdc934b9e058ffd3e401e5550921
Greg Schuman <gcschuman@hotmail.com>
2023-03-20 12:51:07 -0700

    Added a few filler quotes in the script.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const generateQuote = function(){
     const quotes = [
-        quote: "A shit show at the fuck factory"
-    ],
-    
+        {quote: "A shit show at the fuck factory"},
+        {quote: "He said to his son as the sexual assault allegations came rolling in"},
+        {quote: "Well, fuck me to your fucking fuck"}
+    ]
+
 }

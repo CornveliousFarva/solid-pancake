7212dd66e4ea0c31757c85e9946eebdb2720cca7
Greg Schuman <gcschuman@hotmail.com>
2023-03-21 13:06:07 -0700

    Added a commented out quote in the script to be filled in later.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const generateQuote = function(){
     const quotes = [
         {quote: "A shit show at the fuck factory"},
         {quote: "He said to his son as the sexual assault allegations came rolling in"},
-        {quote: "Well, fuck me to your fucking fuck"}
+        {quote: "Well, fuck me to your fucking fuck"},
+        // {quote: ""},
     ]
 
 }

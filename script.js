10161dd6c299e707215e1353c862f711688ad953
Greg Schuman <gcschuman@hotmail.com>
2023-03-21 13:17:35 -0700

    Added the remainder of the script, loading and generating the quote.

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const generateQuote = function(){
         // {quote: ""},
     ]
 
+    let arrayIndex = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quotes").innerHTML = quotes[arrayIndex].quote;
+
+}
+
+window.onload = function(){
+    generateQuote();
+    document.getElementById("generate").addEventListener('click', generateQuote);
 }
 
 // As I'm pulling from a single character rather than various sources, I'm having to add the quotes in manually.
